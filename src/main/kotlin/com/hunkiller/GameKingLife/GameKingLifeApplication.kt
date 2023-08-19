681e39741c95684ddab9e013665369a09599a142
Jose <josediaz4297@gmail.com>
2023-08-19 01:39:55 -0500

    Init Commit

---
+++ src/main/kotlin/com/hunkiller/GameKingLife/GameKingLifeApplication.kt
@@ -0,0 +1,11 @@
+package com.hunkiller.GameKingLife
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameKingLifeApplication
+
+fun main(args: Array<String>) {
+	runApplication<GameKingLifeApplication>(*args)
+}
---
+++ src/test/kotlin/com/hunkiller/GameKingLife/GameKingLifeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hunkiller.GameKingLife
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameKingLifeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
